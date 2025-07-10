1bb42999e230f08efd0d9c7785f3d62c57dbe3ed
Maksiks <maksiksking@gmail.com>
2025-07-10 22:57:20 +0300

    feat: that's it

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<main>
+<h1>Here have a link to the bot:<br>
+    <a href="https://discord.com/oauth2/authorize?client_id=1392818641359212667&permissions=277025393664&integration_type=0&scope=bot+applications.commands">
+        click me
+    </a>
+    <br>
+    <br>
+    also, have a bagel, it's a voting bribe:<br>
+    🥯
+</h1>
+</main>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap');
+
+    :global {
+        html, body {
+            overflow: hidden;
+            color: white;
+            background: #222222;
+            font-family: 'Comfortaa', sans-serif;
+        }
+    }
+
+    main {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        & h1 {
+            text-align: center;
+
+            & a {
+                font-size: 1.5rem;
+                color: #c000ff;
+            }
+        }
+    }
+</style>
